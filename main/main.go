940f7f21c75bef6ea769619c36b5f5a16703cf65
Teodor B <tbardici@gmail.com>
2017-03-26 21:25:33 +0200

    command line arguments

--- main/main.go
+++ main/main.go
@@ -4,7 +4,11 @@ import (
 	"curioz/api"
 	"curioz/cameras"
 	"curioz/worker"
+	"errors"
+	"flag"
 	"fmt"
+	"os"
+	"path"
 )
 
 type camera struct {
@@ -14,15 +18,95 @@ type camera struct {
 
 func main() {
 
-	sol := 1500
-	for sol <= 1600 {
-		response, err := api.Get(sol, cameras.NAVCAM)
+	argSol := flag.Int("sol", -1, "sol - martian day")
+	argCam := flag.String("cam", "MAST", "Curiosity rover camera")
+	argDest := flag.String("dest", "", "Destination directory")
+
+	flag.Parse()
+
+	sol := *argSol
+	cam := *argCam
+	dest := *argDest
+
+	if sol == -1 {
+		printHelp()
+		return
+	}
+
+	if cam != cameras.FHAZ &&
+		cam != cameras.RHAZ &&
+		cam != cameras.MAST &&
+		cam != cameras.CHEMCAM &&
+		cam != cameras.MAHLI &&
+		cam != cameras.MARDI &&
+		cam != cameras.NAVCAM &&
+		cam != cameras.PANCAM {
+		printHelp()
+		return
+	}
+
+	err := ensureDestinationIsCreated(&dest)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for sol <= sol+50 {
+
+		response, err := api.Get(sol, cam)
 		if err != nil {
 			fmt.Println(err)
 			break
 		}
-		worker.Fetch(sol, cameras.MAST, response)
+
+		err = worker.Fetch(sol, cam, response, dest)
+		if err != nil {
+			fmt.Println(err)
+		}
+
 		sol++
 	}
 
+}
+
+func ensureDestinationIsCreated(dest *string) (err error) {
+	if len(*dest) == 0 {
+		cwd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		*dest = path.Join(cwd, "img_result")
+
+		//create dest dir if it does not exists
+		if _, err := os.Stat(*dest); os.IsNotExist(err) {
+			err = os.MkdirAll(*dest, 0777)
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		if _, err := os.Stat(*dest); os.IsNotExist(err) {
+			return errors.New("Destination does not exist")
+		}
+	}
+
+	return nil
+}
+
+func printHelp() {
+	fmt.Println("Arguments:")
+	fmt.Printf("\t--sol : the martian day for which you want to download pictures\n")
+	fmt.Printf("\t--cam : mars rover rover camera")
+	fmt.Printf("\tCameras:\n")
+	fmt.Printf("\t\tFHAZ - Front Hazard Avoidance Camera\n")
+	fmt.Printf("\t\tRHAZ - Rear Hazard Avoidance Camera\n")
+	fmt.Printf("\t\tMAST - Mast Camera\n")
+	fmt.Printf("\t\tCHEMCAM - Chemistry and Camera Complex\n")
+	fmt.Printf("\t\tMAHLI - Mars Hand Lens Imager\n")
+	fmt.Printf("\t\tMARDI - Mars Descent Imager\n")
+	fmt.Printf("\t\tNAVCAM - Navigation Camera\n")
+	fmt.Printf("\t\tPANCAM - Panoramic Camera\n")
+	fmt.Printf("Usage example:\n")
+	fmt.Printf("\tcurioz --sol=1000 --cam=FHAZ")
 }
--- worker/worker.go
+++ worker/worker.go
@@ -6,22 +6,26 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 	"sync"
 )
 
+// CONCURRENCY is the number of threads
+const CONCURRENCY = 6
+
 var wg sync.WaitGroup
 
 //Fetch fetches all images from the given API response
-func Fetch(sol int, camera string, response api.Response) (err error) {
+func Fetch(sol int, camera string, response api.Response, dest string) (err error) {
 
 	fmt.Println("SOL: ", sol)
 
 	//channel to use as a semaphore.
 	//its capacity of 6 will block until a task finishes and removes from it
 	//
-	sem := make(chan bool, 6)
+	sem := make(chan bool, CONCURRENCY)
 
 	if len(response.Photos) == 0 {
 		fmt.Printf("Sol %d: no pictures.\n", sol)
@@ -31,55 +35,49 @@ func Fetch(sol int, camera string, response api.Response) (err error) {
 	for _, photo := range response.Photos {
 		sem <- true
 		wg.Add(1)
-		go func(s int, c string, i string) {
+		go func(imgSrc string) {
 			defer func() { <-sem }()
-			fetchImage(s, c, i)
-		}(sol, camera, photo.ImgSrc)
+
+			fullfn := getFullFN(sol, camera, imgSrc, dest)
+			if fullfn == "" {
+				return
+			}
+
+			bytes, err := fetchImage(sol, camera, imgSrc)
+			if err != nil {
+				return
+			}
+
+			err = writeFile(bytes, fullfn)
+			if err != nil {
+				os.Exit(0)
+			}
+
+		}(photo.ImgSrc)
 	}
 
 	return nil
 }
 
-func fetchImage(sol int, camera string, url string) (err error) {
-
-	//wait for me!
-	defer wg.Done()
-
-	fmt.Println(url)
+func getFullFN(sol int, camera string, url string, dest string) (fullfn string) {
 
 	//build path of output file;
 	index := strings.LastIndex(url, "/") + 1
 	fn := "sol_" + strconv.Itoa(sol) + "_" + camera + "_" + url[index:]
-	cwd, er := os.Getwd()
-	if er != nil {
-		return er
-	}
-	//full file name
-	fullfn := cwd + "\\..\\img_result\\" + fn
+
+	fullfn = path.Join(dest, fn)
 
 	//check if file already exists; if yes, return
-	_, er = os.Stat(fullfn)
+	_, er := os.Stat(fullfn)
 	if er == nil {
 		fmt.Printf("File exists: %s\n", fn)
-		return nil
+		return ""
 	}
 
-	webResponse, er := http.Get(url)
-	if er != nil {
-		return er
-	}
+	return fullfn
+}
 
-	//defer closing of stream
-	defer webResponse.Body.Close()
-
-	//read response into byte array
-	bytes, er := ioutil.ReadAll(webResponse.Body)
-	if er != nil {
-		return er
-	}
-
-	fmt.Printf("worker fetched %d bytes\n", len(bytes))
-
+func writeFile(bytes []byte, fullfn string) (err error) {
 	//create new file
 	f, er := os.Create(fullfn)
 	if er != nil {
@@ -87,12 +85,39 @@ func fetchImage(sol int, camera string, url string) (err error) {
 	}
 
 	//write bytes to file
-	_, er = f.Write(bytes)
-	if er != nil {
-		return er
+	_, err = f.Write(bytes)
+	if err != nil {
+		return
 	}
 
 	f.Close()
 
 	return nil
+}
+
+func fetchImage(sol int, camera string, url string) (bytes []byte, err error) {
+
+	//wait for me!
+	defer wg.Done()
+
+	fmt.Println(url)
+
+	//get response
+	webResponse, er := http.Get(url)
+	if er != nil {
+		return nil, er
+	}
+
+	//defer closing of stream
+	defer webResponse.Body.Close()
+
+	//read response into byte array
+	bytes, er = ioutil.ReadAll(webResponse.Body)
+	if er != nil {
+		return nil, er
+	}
+
+	fmt.Printf("worker fetched %d bytes\n", len(bytes))
+
+	return bytes, nil
 }
